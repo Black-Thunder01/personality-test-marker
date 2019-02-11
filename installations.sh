f1f2db0a80811bbc9d3c8eca6cb77a51e8e10e38
Black_Thunder01 <Johankhanye01@gmail.com>
2019-02-11 11:36:26 +0200

    Add an installation file to install all requirements

---
+++ installations.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pip install pandas==0.23.4
+pip install Cython
+pip install jupyter==1.0.0
+pip install numpy==1.16.0
+pip install Click==7.0
+pip install gspread==3.1.0
+
+pip install pyOpenSSL==19.0.0
+
+pip install argparse
+pip install oauth2client==1.5.2
+pip install pandas
+pip install numpy
+pip install functools
+
